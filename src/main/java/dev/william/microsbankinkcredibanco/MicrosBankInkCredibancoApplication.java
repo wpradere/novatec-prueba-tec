f03ce5d0d22b9ba6aba6b26ebfcac7efc8fd919f
WilliKahan <skaoi39@gmail.com>
2024-05-20 18:48:17 -0500

    Primer commit inicio proyecto

---
+++ src/main/java/dev/william/microsbankinkcredibanco/MicrosBankInkCredibancoApplication.java
@@ -0,0 +1,13 @@
+package dev.william.microsbankinkcredibanco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrosBankInkCredibancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrosBankInkCredibancoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/william/microsbankinkcredibanco/MicrosBankInkCredibancoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.william.microsbankinkcredibanco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrosBankInkCredibancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
